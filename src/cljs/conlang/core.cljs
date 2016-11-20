d3e614193df23802a872522e14dd13c3f925b6a8
maxbittker <maxbittker@gmail.com>
2016-11-20 02:54:16 -0500

    lookscool

--- src/cljs/conlang/core.cljs
+++ src/cljs/conlang/core.cljs
@@ -6,30 +6,59 @@
 
 ;; -------------------------
 ;; Views
+
+(def input-word (reagent/atom "test"))
+
+(defn counting-component []
+  [:div
+   [:input {:type "text" :value @input-word
+            :on-change #(reset! input-word (-> % .-target .-value))}]])
+
 (defn format-points [points]
   (clojure.string/join " " (map #(clojure.string/join "," %) points)))
 
 (defn random-points []
-  (take (* 100 (Math/random) (Math/random))
+  (take 700
    (repeatedly
      (fn []
-       [(dec (* (Math/random) (Math/random) 250))
-        (dec (* (Math/random) (Math/random) 250))]))))
+       [(dec (*  (Math/random)(Math/random) 250))
+        (dec (*  (Math/random)(Math/random) 250))]))))
 
+(def pts (reagent/atom (random-points)))
+
+(defn vib [points]
+  (map (fn [[x y]]
+        [(+ x (- 0.5 (Math/random)))
+         (+ y (- 0.5 (Math/random)))])
+       points))
+
+(defn warp [points]
+ (map (fn [[x y]]
+       [(mod (- x 0.99) 250)
+        (mod (- y 0.99) 250)])
+      points))
+
+(defn updater []
+  (swap! pts (comp vib warp))
+  (. js/window (requestAnimationFrame updater)))
+
+(updater)
 
 (defn home-page []
-  (let [pts (random-points)]
+  (let [formated (format-points @pts)]
    [:div {:class "display"}
+    ; [counting-component]
     [:svg {:width 500 :height 500}
       [:g {:transform "translate(250, 250)"}
         [:g {:transform "rotate(0, 0, 0)"}
-         [:polygon {:points (format-points pts)}]]
+         [:polygon {:points formated}]]
         [:g {:transform "rotate(90, 0, 0)"}
-         [:polygon {:points (format-points pts)}]]
+         [:polygon {:points formated}]]
         [:g {:transform "rotate(180 ,0, 0)"}
-         [:polygon {:points (format-points pts)}]]
+         [:polygon {:points formated}]]
         [:g {:transform "rotate(270, 0, 0)"}
-         [:polygon {:points (format-points pts)}]]]]]))
+         [:polygon {:points formated}]]]]]))
+
 
 (defn about-page []
   [:div [:h2 "About conlang"]
